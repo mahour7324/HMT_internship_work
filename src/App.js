66a5a37086cb117cc55d6d00dd2d578b6e2e6992
mahour7324 <mahour7324@gmail.com>
2023-04-11 10:00:41 +0530

    html is working now

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Home from "./pages/Home"
+import { BrowserRouter , Route , Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
+    <div classNameName="App">
 {/*-------------------------------------------------------------------------------------------------------------------------------------- */}
-    
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>} />
 
+    </Routes>
 
+    </BrowserRouter>
 
 
 
